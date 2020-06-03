32e1f586e73341365a38f0e5b774910b2044efe7
Oriana <oriana.nohemi22@gmail.com>
2020-06-03 17:23:09 -0500

    Enmascarar tarjeta

--- src/index.js
+++ src/index.js
@@ -1,36 +1,22 @@
 import validator from './validator.js';
 
-console.log(validator);
-
-document.getElementById("button").addEventListener("click", tomarNumerodeTarjeta )
+document.getElementById("button").addEventListener("click", tomarNumerodeTarjeta)
 
 function tomarNumerodeTarjeta() {
     let numeroDeTarjeta = document.getElementById("inputNumber").value
     if(numeroDeTarjeta == "") {
         alert("ERROR!!.. El campo debe tener un valor numerico");
-    } else if (numeroDeTarjeta.length == 15){
+    } else if (numeroDeTarjeta.length !== 16){
         alert("El campo debe contener 16 numeros");
     } else {
-        invertirNumeroDeTarjeta(numeroDeTarjeta);
-    }
-    console.log(numeroDeTarjeta)
-}
-
-function invertirNumeroDeTarjeta(numeroDeTarjeta){
-    let numeroDeTarjetaInvertido = ""
-    let numerosParesPrueba = []
-    for(let i = 15; i>=0; i--){
-        numeroDeTarjetaInvertido += numeroDeTarjeta[i]
-        if(i % 2 == 1) {
-            numerosParesPrueba += numeroDeTarjeta[i] 
+        let valido = validator.isValid(numeroDeTarjeta)
+        if(valido) {
+            validator.maskify(numeroDeTarjeta)
+            console.log(validator.maskify(numeroDeTarjeta))
+        } else {
+            alert("Tarjeta de Credito No valido")
         }
     }
-    console.log(numeroDeTarjetaInvertido)
-    console.log(numerosParesPrueba)
-}
+  }
 
-// function numerosPares(numeroDeTarjetaInvertido){
-//     let numberPares = numeroDeTarjetaInvertido.split('')
-//     console.log(numberPares)
-
-// }
+console.log(validator);
